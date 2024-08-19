9bcf1b18681d26cc8c8a80ebde84e46e487e56d1
yallo428 <116006875+yallo428@users.noreply.github.com>
2024-08-19 16:32:29 +0900

    [#10] Docs init controller creatTeam

---
+++ src/test/java/soon/PTCMR_Back/domain/team/controller/TeamControllerDocsTest.java
@@ -0,0 +1,55 @@
+package soon.PTCMR_Back.domain.team.controller;
+
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import soon.PTCMR_Back.domain.team.dto.reqeust.TeamCreateRequest;
+
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "dns-name.com", uriPort = 443)
+@AutoConfigureMockMvc
+@ExtendWith(RestDocumentationExtension.class)
+@SpringBootTest
+public class TeamControllerDocsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("[POST] /api/v1/team 요청 시 팀 생성")
+    void create() throws Exception {
+        TeamCreateRequest teamCreateRequest = new TeamCreateRequest("test");
+
+        String json = objectMapper.writeValueAsString(teamCreateRequest);
+
+        mockMvc.perform(post("/api/v1/team")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+            .andExpect(status().isCreated())
+            .andDo(print())
+            .andDo(document("team-create",
+                requestFields(
+                    fieldWithPath("title").description("팀 이름")
+                )));
+
+    }
+
+}
