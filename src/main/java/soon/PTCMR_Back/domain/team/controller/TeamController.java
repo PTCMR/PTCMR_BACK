7fccb3c29cb9a53c64749c9bf9b2066190842aeb
yallo428 <yallo428@naver.com>
2024-08-20 05:37:27 +0900

    [#10] Refactor TeamController 유저 파라미터 추가

--- src/main/java/soon/PTCMR_Back/domain/team/controller/TeamController.java
+++ src/main/java/soon/PTCMR_Back/domain/team/controller/TeamController.java
@@ -4,12 +4,14 @@ package soon.PTCMR_Back.domain.team.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import soon.PTCMR_Back.domain.team.dto.reqeust.TeamCreateRequest;
 import soon.PTCMR_Back.domain.team.service.TeamService;
+import soon.PTCMR_Back.global.oauth.CustomOAuth2User;
 
 @RestController
 @RequestMapping("/api/v1/team")
@@ -20,9 +22,10 @@ public class TeamController {
 
 	@PostMapping
 	public ResponseEntity<Long> createTeam(
-		@RequestBody final TeamCreateRequest teamCreateRequest
+		@RequestBody final TeamCreateRequest teamCreateRequest,
+		@AuthenticationPrincipal CustomOAuth2User user
 	) {
 		return ResponseEntity.status(HttpStatus.CREATED)
-			.body(teamService.create(teamCreateRequest.title()));
+			.body(teamService.create(teamCreateRequest.title(), user.getUUID()));
 	}
 }
