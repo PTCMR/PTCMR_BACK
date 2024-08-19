b7e394acb23cff125d67d59c0ecd5bfffa25adac
yallo428 <yallo428@naver.com>
2024-08-20 05:36:43 +0900

    [#10] Feat TeamMember 생성 구현

--- src/main/java/soon/PTCMR_Back/domain/team/service/TeamService.java
+++ src/main/java/soon/PTCMR_Back/domain/team/service/TeamService.java
@@ -3,6 +3,8 @@ package soon.PTCMR_Back.domain.team.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import soon.PTCMR_Back.domain.member.entity.Member;
+import soon.PTCMR_Back.domain.member.repository.MemberRepository;
 import soon.PTCMR_Back.domain.team.entity.Team;
 import soon.PTCMR_Back.domain.team.repository.TeamRepository;
 
@@ -12,13 +14,17 @@ import soon.PTCMR_Back.domain.team.repository.TeamRepository;
 public class TeamService {
 
     private final TeamRepository teamRepository;
-
+    private final MemberRepository memberRepository;
+    private final TeamManager teamManager;
 
     @Transactional
-    public Long create(String title) {
+    public Long create(String uuid, String title) {
+        Member member = memberRepository.findByUuid(uuid);
         Team team = Team.create(title);
 
         Long result = teamRepository.save(team);
+
+        teamManager.joinTeam(team, member);
 
         return result;
     }
