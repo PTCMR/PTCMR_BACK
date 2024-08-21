b5ce955944cb6cf7d6d4be387b710f90f0a7ef92
yallo428 <yallo428@naver.com>
2024-08-21 14:47:24 +0900

    [#17] Feat TeamController updateTeam

--- src/main/java/soon/PTCMR_Back/domain/team/controller/TeamController.java
+++ src/main/java/soon/PTCMR_Back/domain/team/controller/TeamController.java
@@ -6,10 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import soon.PTCMR_Back.domain.team.dto.reqeust.TeamCreateRequest;
+import soon.PTCMR_Back.domain.team.dto.reqeust.TeamUpdateRequest;
+import soon.PTCMR_Back.domain.team.dto.response.TeamDetails;
 import soon.PTCMR_Back.domain.team.service.TeamService;
 import soon.PTCMR_Back.global.oauth.CustomOAuth2User;
 
@@ -27,5 +30,20 @@ public class TeamController {
 	) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 			.body(teamService.create(teamCreateRequest.title(), user.getUUID()));
+	}
+
+	@PutMapping
+	public ResponseEntity<TeamDetails> updateTeam(
+		@RequestBody final TeamUpdateRequest teamUpdateRequest,
+		@AuthenticationPrincipal CustomOAuth2User user
+	) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(teamService.update(
+				teamUpdateRequest.teamId(),
+				teamUpdateRequest.newTitle(),
+				teamUpdateRequest.notificationDay(),
+				teamUpdateRequest.notificationHour(),
+				user.getUUID()
+			));
 	}
 }
