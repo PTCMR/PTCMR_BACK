e0e3e4c9768eefaa1dabf35ec716f5fe21f059d8
yallo428 <yallo428@naver.com>
2024-08-21 21:40:37 +0900

    [#20] Feat TeamController deleteTeam

--- src/main/java/soon/PTCMR_Back/domain/team/controller/TeamController.java
+++ src/main/java/soon/PTCMR_Back/domain/team/controller/TeamController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,5 +47,14 @@ public class TeamController {
 				teamUpdateRequest.notificationHour(),
 				user.getUUID()
 			));
+	}
+
+	@DeleteMapping("/{teamId}")
+	public ResponseEntity<Void> deleteTeam(
+		@PathVariable final Long teamId,
+		@AuthenticationPrincipal CustomOAuth2User user
+	){
+		teamService.delete(teamId, user.getUUID());
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
