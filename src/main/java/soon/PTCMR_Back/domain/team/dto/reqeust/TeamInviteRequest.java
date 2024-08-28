f6246724f37ef43af1e3c2790111c27f64fa79bf
yallo428 <yallo428@naver.com>
2024-08-28 23:36:34 +0900

    [#26] Refactor Team Dto Add Validator annotation

--- src/main/java/soon/PTCMR_Back/domain/team/dto/reqeust/TeamInviteRequest.java
+++ src/main/java/soon/PTCMR_Back/domain/team/dto/reqeust/TeamInviteRequest.java
@@ -1,6 +1,9 @@
 package soon.PTCMR_Back.domain.team.dto.reqeust;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record TeamInviteRequest(
+	@NotBlank(message = "초대 코드는 필수입니 다.")
 	String inviteCode
 ) {
 
