aa982b0d914cae712bcd8ed279516d576a373523
jjh75607 <dkrak5738@naver.com>
2024-09-03 15:43:00 +0900

    [#34] Feat Category Controller 구현

--- src/main/java/soon/PTCMR_Back/domain/category/controller/CategoryController.java
+++ src/main/java/soon/PTCMR_Back/domain/category/controller/CategoryController.java
@@ -1,14 +1,19 @@
 package soon.PTCMR_Back.domain.category.controller;
 
 import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import soon.PTCMR_Back.domain.category.dto.request.CategoryCreateRequest;
+import soon.PTCMR_Back.domain.category.dto.request.CategoryUpdateRequest;
 import soon.PTCMR_Back.domain.category.service.CategoryService;
 
 @RequiredArgsConstructor
@@ -23,5 +28,18 @@ public class CategoryController {
         Long categoryId = categoryService.create(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(categoryId);
+    }
+
+    @PatchMapping("/{categoryId}")
+    public ResponseEntity<Void> update(@RequestBody @Valid CategoryUpdateRequest request,
+        @PathVariable Long categoryId) {
+        categoryService.update(request, categoryId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/api/v1/category/"));
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+            .headers(headers)
+            .build();
     }
 }
--- src/main/java/soon/PTCMR_Back/domain/category/entity/Category.java
+++ src/main/java/soon/PTCMR_Back/domain/category/entity/Category.java
@@ -12,11 +12,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import soon.PTCMR_Back.domain.product.entity.Product;
 import soon.PTCMR_Back.domain.team.entity.Team;
 
-@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
